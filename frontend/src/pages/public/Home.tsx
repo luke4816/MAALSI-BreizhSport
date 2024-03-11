c087648e031b0738f225276bcc52d02299fd69ff
luke4816 <luc.bournel@viacesi.fr>
2024-03-11 09:37:25 +0100

    fix(homepage): fix title

--- frontend/src/pages/public/Home.tsx
+++ frontend/src/pages/public/Home.tsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 export default function Home() {
     return (
         <Container>
-            <h1>Bienvenue sur le site e-commerce de Breizh Sport</h1>
+            <h1>Bienvenue sur le site de Breizh Sport</h1>
         </Container>
     );
 }
